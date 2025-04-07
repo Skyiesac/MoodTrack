c87ea7be34cc922dae86e3c296bcd59c02d75c97
Skyiesac <jainsachi1202@gmail.com>
2025-04-07 21:06:56 +0530

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,15 @@
-import { drizzle } from "drizzle-orm/neon-serverless";
-import ws from "ws";
-import * as schema from "@db/schema";
+import { Sequelize } from "sequelize";
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
+    "DATABASE_URL must be set. Did you forget to provision a database?"
   );
 }
 
-export const db = drizzle({
-  connection: process.env.DATABASE_URL,
-  schema,
-  ws: ws,
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false, // Disable logging, set to console.log for debugging
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === "production", // Use SSL in production
+  },
 });
