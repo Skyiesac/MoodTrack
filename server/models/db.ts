d4d42c1c9461b38991a6e561d24d091d3c0f4068
Skyiesac <jainsachi1202@gmail.com>
2025-04-07 19:26:58 +0530

    Added Sequelize

--- server/models/db.ts
+++ server/models/db.ts
@@ -1,9 +1,27 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+// Load environment variables from .env file
+dotenv.config();
+
+console.log('Environment Variables:', process.env);
+
+// Ensure the DATABASE_URL environment variable is set
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  console.error('DATABASE_URL is not defined or is invalid');
+  throw new Error('DATABASE_URL is not defined or is invalid');
+}
+
+console.log('Database URL:', databaseUrl);
+
+const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
     ssl: process.env.NODE_ENV === 'production'
   }
 });
+
+export default sequelize;
